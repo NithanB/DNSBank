6d55bd8280f0ff7002b95f1fda48d358be7d074c
Nithan Panyos <41776801+NithanB@users.noreply.github.com>
2025-09-09 16:09:34 +0700

    Main Menu

--- app/src/main/java/com/example/dnsbank3/startMenu.kt
+++ app/src/main/java/com/example/dnsbank3/startMenu.kt
@@ -1,31 +1,20 @@
 package com.example.dnsbank3
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class startMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start_menu)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        
-        val startUp : Button = findViewById(R.id.startbtn)
-        
-        startUp.setOnClickListener {
-            val intent = Intent(this, startMenu::class.java)
-            startActivity(intent)
-            Log.d("MainActivity", "Button Clicked, Starting startMenu")
         }
     }
 }
