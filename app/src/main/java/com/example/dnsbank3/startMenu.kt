7939116083847976c7ec0d5c324320ea8a22d208
Nithan Panyos <41776801+NithanB@users.noreply.github.com>
2025-09-10 14:43:03 +0700

    Start Menu Debug
    Start Menu Debugged

--- app/src/main/java/com/example/dnsbank3/startMenu.kt
+++ app/src/main/java/com/example/dnsbank3/startMenu.kt
@@ -1,6 +1,9 @@
 package com.example.dnsbank3
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,37 @@ class startMenu : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val centralRegion : Button = findViewById(R.id.buttonCentralRegion)
+        val northEastRegion : Button = findViewById(R.id.buttonNortheastRegion)
+        val northRegion : Button = findViewById(R.id.buttonNorthRegion)
+        val southernRegion : Button = findViewById(R.id.buttonSouthRegion)
+
+        centralRegion.setOnClickListener {
+            val intent = Intent(this, com.example.dnsbank3.centralRegion::class.java)
+            startActivity(intent)
+            Log.d("startMenu", "Button Clicked, Starting centralRegion")
+        }
+        northEastRegion.setOnClickListener {
+            val intent = Intent(this, com.example.dnsbank3.northEastRegion::class.java)
+            startActivity(intent)
+            Log.d("startMenu", "Button Clicked, Starting northEastRegion")
+        }
+        northRegion.setOnClickListener {
+            val intent = Intent(this, com.example.dnsbank3.northRegion::class.java)
+            startActivity(intent)
+            Log.d("startMenu", "Button Clicked, Starting northRegion")
+        }
+        southernRegion.setOnClickListener {
+            val intent = Intent(this, com.example.dnsbank3.southernRegion::class.java)
+            startActivity(intent)
+            Log.d("startMenu", "Button Clicked, Starting southernRegion")
+        }
+
+
+
+
+
+
     }
 }
