3cc1eb1180c1ac8c828c22f214389625345c67e0
Nithan Panyos <41776801+NithanB@users.noreply.github.com>
2025-09-11 10:18:02 +0700

    Main Menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.dnsbank3"
-        minSdk = 35
+        minSdk = 31
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
